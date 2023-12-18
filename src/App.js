deb9a09ca63e240559693cdd43b47d75657e8a24
Hayotbek-Turgunov <turgunovhayotbek12@gmail.com>
2023-12-18 19:56:04 +0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Businnes from './Component/Businnes/Businnes'
 import ColBannerCard from './Component/ColBannerCard/ColBannerCard'
 import EventsCard from './Component/EventsCard/EventsCard'
 import Aside from './Component/Aside/Aside'
-
+import Footer from './Component/Footer/Footer'
 
 function App() {
     return (
@@ -16,6 +16,7 @@ function App() {
             <ColBannerCard />
             <EventsCard />
             <Aside />
+            <Footer />
         </div>
     );
 }
---
+++ src/Component/Footer/Footer.js
@@ -0,0 +1,182 @@
+import './Footer.css'
+import { FaInstagram } from "react-icons/fa6";
+import { FaYoutube } from "react-icons/fa";
+import { FaFacebookF } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaLinkedinIn } from "react-icons/fa6";
+import { FaPinterest } from "react-icons/fa";
+import { FaTiktok } from "react-icons/fa";
+import { SiAtandt } from "react-icons/si";
+
+
+function Footer() {
+    return (
+        <div className='footer'>
+            <div className="events-card-container">
+                <div className="footer-container">
+                    <div className="footer-shop-learn">
+                        <p className='footer-shop-learn-heading'>Shop and Learn</p>
+                        <div className="footer-shop-learn-wrapper-item">
+                            <ul className='footer-shop-learn-list'>
+                                <li className='footer-shop-learn-item'><span>Mobile/ Handlers</span></li>
+                                <li className='footer-shop-learn-item'><a className='footer-shop-learn-links' href="">Phones</a></li>
+                                <li className='footer-shop-learn-item'><a className='footer-shop-learn-links' href="">Accessories</a></li>
+                                <li className='footer-shop-learn-item'><a className='footer-shop-learn-links' href="">Accessories</a></li>
+                                <li className='footer-shop-learn-item'><span>Laptops</span></li>
+                                <li className='footer-shop-learn-item'><a className='footer-shop-learn-links' href="">For Home</a></li>
+                                <li className='footer-shop-learn-item'><a className='footer-shop-learn-links' href="">For Work</a></li>
+                                <li className='footer-shop-learn-item'><a className='footer-shop-learn-links' href="">For Creators</a></li>
+                                <li className='footer-shop-learn-item'><a className='footer-shop-learn-links' href="">For Students</a></li>
+                                <li className='footer-shop-learn-item'><a className='footer-shop-learn-links' href="">For Gaming</a></li>
+                                <li className='footer-shop-learn-item'><a className='footer-shop-learn-links' href="">Accessories</a></li>
+                                <li className='footer-shop-learn-item'><span>Displays / Desktops</span></li>
+                                <li className='footer-shop-learn-item'><a className='footer-shop-learn-links' href="">Monitors</a></li>
+                                <li className='footer-shop-learn-item'><a className='footer-shop-learn-links' href="">Projectors</a></li>
+                                <li className='footer-shop-learn-item'><a className='footer-shop-learn-links' href="">All-in-One PCs</a></li>
+                                <li className='footer-shop-learn-item'><a className='footer-shop-learn-links' href="">Tower PCs</a></li>
+                                <li className='footer-shop-learn-item'><a className='footer-shop-learn-links' href="">Gaming Tower PCs</a></li>
+                            </ul>
+
+                            <ul className='footer-shop-learn-list'>
+                                <li className='footer-shop-learn-item'><a className='footer-shop-learn-links' href="">NUCs</a></li>
+                                <li className='footer-shop-learn-item'><a className='footer-shop-learn-links' href="">Mini PCs</a></li>
+                                <li className='footer-shop-learn-item'><a className='footer-shop-learn-links' href="">Workstadions</a></li>
+                                <li className='footer-shop-learn-item'><a className='footer-shop-learn-links' href="">Accessories</a></li>
+                                <li className='footer-shop-learn-item'><span>Motherboards / Components</span></li>
+                                <li className='footer-shop-learn-item'><a className='footer-shop-learn-links' href="">Motherboards</a></li>
+                                <li className='footer-shop-learn-item'><a className='footer-shop-learn-links' href="">Graphics Cards</a></li>
+                                <li className='footer-shop-learn-item'><a className='footer-shop-learn-links' href="">Gaming Cases</a></li>
+                                <li className='footer-shop-learn-item'><a className='footer-shop-learn-links' href="">Colling</a></li>
+                                <li className='footer-shop-learn-item'><a className='footer-shop-learn-links' href="">Power Supply Units</a></li>
+                                <li className='footer-shop-learn-item'><a className='footer-shop-learn-links' href="">Sound Cards</a></li>
+                                <li className='footer-shop-learn-item'><a className='footer-shop-learn-links' href="">Optical Drives</a></li>
+                                <li className='footer-shop-learn-item'><a className='footer-shop-learn-links' href="">Data Storage</a></li>
+                                <li className='footer-shop-learn-item'><a className='footer-shop-learn-links' href="">External Graphics Docks</a></li>
+                                <li className='footer-shop-learn-item'><span>Networking / IoT / Servers</span></li>
+                                <li className='footer-shop-learn-item'><a className='footer-shop-learn-links' href="">WIFI 7</a></li>
+                                <li className='footer-shop-learn-item'><a className='footer-shop-learn-links' href="">WIFI 6</a></li>
+                                <li className='footer-shop-learn-item'><a className='footer-shop-learn-links' href="">WIFI Routers</a></li>
+                                <li className='footer-shop-learn-item'><a className='footer-shop-learn-links' href="">Whole Home Mesh WiFi System</a></li>
+                            </ul>
+
+                            <ul className='footer-shop-learn-list'>
+                                <li className='footer-shop-learn-item'><a className='footer-shop-learn-links' href="">Business Network Solutions</a></li>
+                                <li className='footer-shop-learn-item'><a className='footer-shop-learn-links' href="">Wired Networking</a></li>
+                                <li className='footer-shop-learn-item'><a className='footer-shop-learn-links' href="">Intelligent Robots</a></li>
+                                <li className='footer-shop-learn-item'><a className='footer-shop-learn-links' href="">AIoT & Industrial Solutions</a></li>
+                                <li className='footer-shop-learn-item'><a className='footer-shop-learn-links' href="">Servers</a></li>
+                                <li className='footer-shop-learn-item'><a className='footer-shop-learn-links' href="">Smart Home</a></li>
+                                <li className='footer-shop-learn-item'><span>Accessories</span></li>
+                                <li className='footer-shop-learn-item'><a className='footer-shop-learn-links' href="">Keyboards</a></li>
+                                <li className='footer-shop-learn-item'><a className='footer-shop-learn-links' href="">Mice and Mouse Pads</a></li>
+                                <li className='footer-shop-learn-item'><a className='footer-shop-learn-links' href="">Headsets and Audio</a></li>
+                                <li className='footer-shop-learn-item'><a className='footer-shop-learn-links' href="">Streaming Kits</a></li>
+                                <li className='footer-shop-learn-item'><a className='footer-shop-learn-links' href="">Apparel Bags and Gear</a></li>
+                                <li className='footer-shop-learn-item'><a className='footer-shop-learn-links' href="">Cases and Protection</a></li>
+                                <li className='footer-shop-learn-item'><a className='footer-shop-learn-links' href="">Adapters and Chargers</a></li>
+                                <li className='footer-shop-learn-item'><a className='footer-shop-learn-links' href="">Docks Dongles and Cable</a></li>
+                                <li className='footer-shop-learn-item'><a className='footer-shop-learn-links' href="">Power Banks</a></li>
+                                <li className='footer-shop-learn-item'><a className='footer-shop-learn-links' href="">Controller</a></li>
+                                <li className='footer-shop-learn-item'><a className='footer-shop-learn-links' href="">Gimbal</a></li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div className="footer-shop-learn">
+                        <p className='footer-shop-learn-heading'>Learn More</p>
+                        <div className="footer-shop-learn-wrapper-item">
+                            <ul className='footer-shop-learn-list'>
+                                <li className='footer-shop-learn-item'><a className='footer-shop-learn-links' href="">Asus Design Center</a></li>
+                                <li className='footer-shop-learn-item'><a className='footer-shop-learn-links' href="">ASUSPRO</a></li>
+                                <li className='footer-shop-learn-item'><a className='footer-shop-learn-links' href="">Automotive Solutions</a></li>
+                                <li className='footer-shop-learn-item'><a className='footer-shop-learn-links' href="">AVC Licensing Notice</a></li>
+                                <li className='footer-shop-learn-item'><a className='footer-shop-learn-links' href="">ASUS Blog</a></li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div className="footer-shop-learn">
+                        <p className='footer-shop-learn-heading'>Support</p>
+                        <div className="footer-shop-learn-wrapper-item">
+                            <ul className='footer-shop-learn-list'>
+                                <li className='footer-shop-learn-item'><a className='footer-shop-learn-links' href="">About ASUS</a></li>
+                                <li className='footer-shop-learn-item'><a className='footer-shop-learn-links' href="">News</a></li>
+                                <li className='footer-shop-learn-item'><a className='footer-shop-learn-links' href="">Investor Relations</a></li>
+                                <li className='footer-shop-learn-item'><a className='footer-shop-learn-links' href="">About CSR for global</a></li>
+                                <li className='footer-shop-learn-item'><a className='footer-shop-learn-links' href="">Press Room</a></li>
+                                <li className='footer-shop-learn-item'><a className='footer-shop-learn-links' href="">ASUSTOR Inc.</a></li>
+                                <li className='footer-shop-learn-item'><a className='footer-shop-learn-links' href="">ASUS Cloud Corporation</a></li>
+                                <li className='footer-shop-learn-item'><a className='footer-shop-learn-links' href="">UniMax Electronics Inc.</a></li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div className="footer-shop-learn">
+                        <p className='footer-shop-learn-heading'>About US</p>
+                        <div className="footer-shop-learn-wrapper-item">
+                            <ul className='footer-shop-learn-list'>
+                                <li className='footer-shop-learn-item'><a className='footer-shop-learn-links' href="">About ASUS</a></li>
+                                <li className='footer-shop-learn-item'><a className='footer-shop-learn-links' href="">News</a></li>
+                                <li className='footer-shop-learn-item'><a className='footer-shop-learn-links' href="">Investor Relations</a></li>
+                                <li className='footer-shop-learn-item'><a className='footer-shop-learn-links' href="">About CSR for global</a></li>
+                                <li className='footer-shop-learn-item'><a className='footer-shop-learn-links' href="">Press Room</a></li>
+                                <li className='footer-shop-learn-item'><a className='footer-shop-learn-links' href="">ASUSTOR Inc.</a></li>
+                                <li className='footer-shop-learn-item'><a className='footer-shop-learn-links' href="">ASUS Cloud Corporation</a></li>
+                                <li className='footer-shop-learn-item'><a className='footer-shop-learn-links' href="">UniMax Electronics Inc.</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="site-footer-bottom-social">
+                    <div className="site-footer-bottom-social-wrapper">
+                        <div className="site-footer-bottom-social-item">
+                            <a href='#' className='site-footer-bootom-social-link'><FaInstagram /></a>
+                        </div>
+                        <div className="site-footer-bottom-social-item">
+                            <a href='#' className='site-footer-bootom-social-link'> <FaYoutube /></a>
+                        </div> <div className="site-footer-bottom-social-item">
+                            <a href='#' className='site-footer-bootom-social-link'> <FaFacebookF /></a>
+                        </div> <div className="site-footer-bottom-social-item">
+                            <a href='#' className='site-footer-bootom-social-link'> <FaTwitter /></a>
+                        </div> <div className="site-footer-bottom-social-item">
+                            <a href='#' className='site-footer-bootom-social-link'> <FaLinkedinIn /></a>
+                        </div> <div className="site-footer-bottom-social-item">
+                            <a href='#' className='site-footer-bootom-social-link'><FaPinterest /></a>
+                        </div> <div className="site-footer-bottom-social-item">
+                            <a href='#' className='site-footer-bootom-social-link'><FaTiktok /></a>
+                        </div>
+                    </div>
+                </div>
+                <hr />
+
+
+                <div className="site-footer-bottom-reserved">
+                    <div className="site-footer-bottom-reserved-container">
+                        <div className="site-footer-bottom-reserved-left">
+                            <ul className='site-footer-reserved-left-list'>
+                                <li className='site-footer-reserved-item'>
+                                    ©ASUSTeK Computer Inc. All rights reserved.
+                                </li>
+
+                                <li className='site-footer-reserved-item'>
+                                    <a className='site-footer-reserved-links' href="">Terms of use Notice</a>
+                                </li>
+
+
+                                <li className='site-footer-reserved-item'>
+                                    <a className='site-footer-reserved-links' href="">Privasy of police</a>
+                                </li>
+                            </ul>
+
+                        </div>
+                        <a className='site-footer-reserved-right-links' href=""> <SiAtandt /> Global/English</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+export default Footer
